87dc71f1fcb8cb2bd43dce068a5b802409d236d9
Gokac9 <theabraham9@gmail.com>
2026-08-06 13:16:42 +0200

    sync seperar lineas de productos

--- luis_botello_discount_line/__manifest__.py
+++ luis_botello_discount_line/__manifest__.py
@@ -8,6 +8,10 @@
     'depends': [
         'account',
         'point_of_sale',
+        # Aseguramos que nuestro override de add_product_by_barcode se cargue
+        # después de los módulos POS convencionales de Xtendoo
+        'pos_conventional_core',
+        'pos_conventional_order_barcode',
     ],
     'data': [
         'views/account_move_views.xml',
